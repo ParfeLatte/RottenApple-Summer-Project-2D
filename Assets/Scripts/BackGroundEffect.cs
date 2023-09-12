7e92a8d182209a4a5d6c1f27fa1e1abaebfdf19d
ParfeLatte <128596001+ParfeLatte@users.noreply.github.com>
2023-09-13 00:27:05 +0900

    안개 효과 추가 및 배경 리소스 변경
    BackgroundEffect에 안개 효과들을 추가해주기만 하면 됩니다

--- Assets/Scripts/BackGroundEffect.cs
+++ Assets/Scripts/BackGroundEffect.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class BackGroundEffect : MonoBehaviour
 {
     private bool SunRiseEnd;
+    private bool ClearFogEnd;
     [SerializeField] private GameObject Sun;
-    //[SerializeField] private SpriteRenderer Fog;
+    [SerializeField] private List<SpriteRenderer> Fogs = new List<SpriteRenderer>();
+    [SerializeField] private List<float> reduceValue = new List<float>();
+    [SerializeField] private float ReduceTime;//17초로 잡고 계산
 
     [SerializeField] private Vector3 FirstSunPos;
     [SerializeField] private float FinalSunYPos;
@@ -17,22 +20,55 @@ public class BackGroundEffect : MonoBehaviour
         Sun.transform.position = FirstSunPos;
     }
 
+    private void Start()
+    {
+        for(int i = 0; i < Fogs.Count; i++)
+        {
+            float Value = Fogs[i].color.a;
+            reduceValue.Add(Value);
+        }
+    }
+
     void Update()
     {
+        
         if (!SunRiseEnd)
         {
             SunRise();
+            ClearFog();
         }
     }
 
     private void SunRise()
     {
-        Sun.transform.position += Vector3.up * Time.deltaTime;
+        Sun.transform.position += Vector3.up * 1.5f * Time.deltaTime;
+        
 
         if (Sun.transform.position.y >= FinalSunYPos)
         {
             Sun.transform.position= new Vector3(Sun.transform.position.x, FinalSunYPos, Sun.transform.position.z);
+            PerfectlyClearFog();
             SunRiseEnd = true;
         }
+    }
+
+    private void ClearFog()
+    {
+        if (SunRiseEnd) return;
+        if (ClearFogEnd) return;
+        for (int i = 0; i < Fogs.Count; i++)
+        {
+            float Value = Fogs[i].color.a - (reduceValue[i] / ReduceTime * Time.deltaTime);
+            Fogs[i].color = new Color(1f, 1f, 1f, Value);
+        }
+    }
+
+    private void PerfectlyClearFog()
+    {
+        for (int i = 0; i < Fogs.Count; i++)
+        {
+            Fogs[i].color = new Color(1f, 1f, 1f, 0);
+        }
+        ClearFogEnd = true;
     }
 }
