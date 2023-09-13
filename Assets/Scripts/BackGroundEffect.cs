721585efde7ee480267bfb01b2e836cc6b40d4a1
ParfeLatte <128596001+ParfeLatte@users.noreply.github.com>
2023-09-13 15:40:08 +0900

    배경 효과 수정
    안개가 먼저 걷히고, 대기 시간을 기다렸다가 태양이 올라오도록 수정했습니다.

--- Assets/Scripts/BackGroundEffect.cs
+++ Assets/Scripts/BackGroundEffect.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BackGroundEffect : MonoBehaviour
 {
     private bool SunRiseEnd;
+    private bool StartSunRise;
     private bool ClearFogEnd;
+    private bool isWait;
     [SerializeField] private GameObject Sun;
     [SerializeField] private List<SpriteRenderer> Fogs = new List<SpriteRenderer>();
     [SerializeField] private List<float> reduceValue = new List<float>();
-    [SerializeField] private float ReduceTime;//17초로 잡고 계산
+    [SerializeField] private float ReduceTime;//15초로 한다고 치고
+    [SerializeField] private float SunriseWaitTime;//기다리는 시간
 
     [SerializeField] private Vector3 FirstSunPos;
     [SerializeField] private float FinalSunYPos;
@@ -17,6 +20,9 @@ public class BackGroundEffect : MonoBehaviour
     void Awake()
     {
         SunRiseEnd = false;
+        StartSunRise = false;
+        ClearFogEnd = false;
+        isWait = false;
         Sun.transform.position = FirstSunPos;
     }
 
@@ -31,10 +37,20 @@ public class BackGroundEffect : MonoBehaviour
 
     void Update()
     {
-        
-        if (!SunRiseEnd)
+        if (SunRiseEnd) return;
+
+        if (StartSunRise)
         {
             SunRise();
+        }
+
+        if (isWait)
+        {
+            WaitForSunRise();
+        }
+
+        if (!ClearFogEnd)
+        {
             ClearFog();
         }
     }
@@ -48,18 +64,24 @@ public class BackGroundEffect : MonoBehaviour
         {
             Sun.transform.position= new Vector3(Sun.transform.position.x, FinalSunYPos, Sun.transform.position.z);
             PerfectlyClearFog();
+            StartSunRise = false;
             SunRiseEnd = true;
         }
     }
 
     private void ClearFog()
     {
-        if (SunRiseEnd) return;
         if (ClearFogEnd) return;
         for (int i = 0; i < Fogs.Count; i++)
         {
             float Value = Fogs[i].color.a - (reduceValue[i] / ReduceTime * Time.deltaTime);
+            if(Value <= 0)
+            {
+                PerfectlyClearFog();
+                return;
+            }
             Fogs[i].color = new Color(1f, 1f, 1f, Value);
+            
         }
     }
 
@@ -70,5 +92,17 @@ public class BackGroundEffect : MonoBehaviour
             Fogs[i].color = new Color(1f, 1f, 1f, 0);
         }
         ClearFogEnd = true;
+        isWait = true;
+    }
+
+    private void WaitForSunRise()
+    {
+        if (!ClearFogEnd) return;
+        SunriseWaitTime += Time.deltaTime;
+        if(SunriseWaitTime >= 30f)
+        {
+            isWait = false;
+            StartSunRise = true;
+        }
     }
 }
